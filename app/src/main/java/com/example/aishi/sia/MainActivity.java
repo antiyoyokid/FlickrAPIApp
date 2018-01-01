1051f036fefae54014bc52ba6a1dd9f51884d03a
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-01-01 12:11:14 -0500

    UI branch

--- app/src/main/java/com/example/aishi/sia/MainActivity.java
+++ app/src/main/java/com/example/aishi/sia/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.aishi.sia;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private VideoView mVideoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mVideoView = (VideoView) findViewById(R.id.videoView3);
+        Uri uri = Uri.parse("android.resources://"+getPackageName()+"/"+R.raw.video);
+        mVideoView.setVideoURI(uri);
+        mVideoView.start();
+
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
     }
+
 }
