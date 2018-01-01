7138678ffb7840b0afeec252ce902ed6d0293776
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-01-01 13:30:15 -0500

    Background Video of Welcome Page was added

--- app/src/main/java/com/example/aishi/sia/MainActivity.java
+++ app/src/main/java/com/example/aishi/sia/MainActivity.java
@@ -2,30 +2,30 @@ package com.example.aishi.sia;
 
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private VideoView mVideoView;
+    VideoView mVideoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mVideoView = (VideoView) findViewById(R.id.videoView1);
 
-        mVideoView = (VideoView) findViewById(R.id.videoView3);
-        Uri uri = Uri.parse("android.resources://"+getPackageName()+"/"+R.raw.video);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.output);
+
         mVideoView.setVideoURI(uri);
         mVideoView.start();
 
         mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.setLooping(true);
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
             }
         });
     }
-
 }
