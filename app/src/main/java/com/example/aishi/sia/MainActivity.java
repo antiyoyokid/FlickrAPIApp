ac7204ad842d9117ef6744da3d51e42e6844786f
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-01-02 12:23:19 -0500

    I added the activity after pressing the next button, also made a java class for it. Also made the next button open this activity :)
    Signed-off-by: Aishik Ghosh <aishik.ghosh@outlook.com>

--- app/src/main/java/com/example/aishi/sia/MainActivity.java
+++ app/src/main/java/com/example/aishi/sia/MainActivity.java
@@ -1,22 +1,33 @@
 package com.example.aishi.sia;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     VideoView mVideoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button moveOn = findViewById(R.id.button);
+        moveOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity();
+            }
+        });
         mVideoView = (VideoView) findViewById(R.id.videoView1);
 
-        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.output);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.output);
 
         mVideoView.setVideoURI(uri);
         mVideoView.start();
@@ -27,5 +38,10 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer.setLooping(true);
             }
         });
+    }
+
+    public void openActivity() {
+        Intent intent = new Intent(this, realActivity.class); //wait....this isn't right
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/aishi/sia/realActivity.java
@@ -0,0 +1,13 @@
+package com.example.aishi.sia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class realActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real);
+    }
+}
